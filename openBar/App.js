d180f47df20867ed8a4c1b2f06b549d6f71c61cc
Henrique Heiden <henrique.heiden@gmail.com>
2019-05-12 21:26:48 -0300

    openBar

---
+++ openBar/App.js
@@ -0,0 +1,31 @@
+import{createAppContainer, createStackNavigator} from 'react-navigation';
+
+import LoginScreen from './src/page/LoginScreen';
+
+const AppNavigator = createStackNavigator({
+  'Login':{
+    screen: LoginScreen,
+    navigationOptions:{
+      title:'Bem Vindo !',      
+    }
+  },
+},{
+  defaultNavigationOptions:{
+    title: 'Series !',
+    headerTintColor:'white',    
+    headerStyle:{
+        backgroundColor:'#6ca2f7',
+        borderBottomWidth: 1,
+        borderBottomColor:'#C5C5C5'
+    },
+    headerTitleStyle:{
+      color:'white',
+      fontSize:30,
+    }
+    
+  }
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
